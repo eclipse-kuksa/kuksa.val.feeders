97c530661f9d4df7f63207107bbb46fefc4ca72a
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2023-01-10 15:25:05 +0200

    Added missing licenses
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

--- someip2val/docker/vsomeip/examples/run-client.sh
+++ someip2val/docker/vsomeip/examples/run-client.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#********************************************************************************
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License 2.0 which is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+#*******************************************************************************/
 
 # important: match VSOMEIP_APPLICATION_NAME with VSOMEIP_CONFIGURATION config, must be equal!
 export VSOMEIP_APPLICATION_NAME="client-sample"
