50e013f2655895d17fb89e69b49b65db13d70a68
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2023-02-10 15:57:51 +0200

    Changed licenses to Apache
    - fixed a typo in ephemeral ttl.sh tag
    
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

--- gps2val/gpsd_feeder.py
+++ gps2val/gpsd_feeder.py
@@ -1,19 +1,14 @@
 #! /usr/bin/env python
 
 ########################################################################
-# Copyright (c) 2020 Robert Bosch GmbH
+# Copyright (c) 2020-2023 Contributors to the Eclipse Foundation
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This program and the accompanying materials are made available under the
+# terms of the Apache License 2.0 which is available at
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # SPDX-License-Identifier: Apache-2.0
 ########################################################################
@@ -22,7 +17,6 @@
 # formatted
 # lat,lon
 # All other values will be reported as 0
-
 
 import threading
 import configparser
