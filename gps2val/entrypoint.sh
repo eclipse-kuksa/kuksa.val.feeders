415a2ca1307f2668982d97493e0a8de3b05debf5
Ivan Kirchev <Ivan.Kirchev@bosch.com>
2023-02-10 15:57:51 +0200

    Changed licenses to Apache
    - fixed a typo in ephemeral ttl.sh tag
    
    Signed-off-by: Ivan Kirchev <Ivan.Kirchev@bosch.com>

--- gps2val/entrypoint.sh
+++ gps2val/entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+#********************************************************************************
+# Copyright (c) 2023 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License 2.0 which is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+#*******************************************************************************/
 
-# ENV GPSD_ARGS="-S ${GPSD_LISTENER_PORT} ${GPSD_SOURCE}"
-# CMD gpsd GPSD_ARGS; ./gpsd_feeder.py
-
+# set defaut options if not set from Dockerfile
 [ -z "$GPSD_OPTIONS" ] && GPSD_OPTIONS="-S 2948 udp://0.0.0.0:29998"
 
 gpsd $GPSD_OPTIONS -N -D 2 &
