4611c7678c500b264a55fde0aac269021bd2cbd9
Sven Erik Jeroschewski <svenerik.jeroschewski@bosch.io>
2023-04-19 09:01:01 +0200

    adds processing of header in csv provider

--- csv_provider/README.md
+++ csv_provider/README.md
@@ -1,5 +1,17 @@
 # VSS Provider
-The aim of this script to provide and replay VSS data into a `kuksa.val` databroker. It is possible to define a sequence of VSS signals including timing delays in a CSV-file and then to apply this sequence towards a `kuksa.val` databroker with the provider. 
+The aim of this script to provide and replay VSS data into a `kuksa.val` databroker. It is possible to define a sequence of VSS signals including timing delays in a CSV-file and then to apply this sequence towards a `kuksa.val` databroker with the provider.
+
+## Usage
+The provider requires an installation of Python in version 3 and can be executed with the following commands:
+
+```
+pip install -r requirements.txt
+python provider.py
+```
+
+This assumes a running `kuksa.val` databroker at `127.0.0.1:55555` and a file named `signals.csv` containing the signals to apply. See the section [Arguments](#arguments) for more details on possible arguments and default values.
+
+The provider uses the [kuksa_client]() Python implementation which you need to install on your system, e.g., by applying the [requirement.txt](requirements.txt) with pip.
 
 ## Arguments
 You can start the provider with the following arguments on a command line:
@@ -19,13 +31,13 @@ An example CSV-files is available in [signals.csv](signals.csv) where an example
 current,Vehicle.Speed,48,1
 ```
 
-The delimiter for the CSV-file is the ',' sign and there is no header in the file, meaning that the first line is interpreted as application data. 
+The delimiter for the CSV-file is the ',' sign. The first line is interpreted as header and not as data.
 
 Each line in the csv file consists of the following elements:
 
-| index | description | example |
+| header | description | example |
 | -- | -----------| --|
-|0 | indicates whether to update the current value (`current`) or the target value (`target`) for the signal. | current |
-| 1 | the name of the signal to update | Vehicle.Speed
-| 2 | the new value | 48 |
-| 3 | Indicates the time in seconds which the provider shall wait after processing this signal. This way one can emulate the change of signals over time. | 0 |
+| field | indicates whether to update the current value (`current`) or the target value (`target`) for the signal. | current |
+| signal | the name of the signal to update | Vehicle.Speed
+| value | the new value | 48 |
+| delay | Indicates the time in seconds which the provider shall wait after processing this signal. This way one can emulate the change of signals over time. | 0 |
--- csv_provider/provider.py
+++ csv_provider/provider.py
@@ -10,18 +10,27 @@ from kuksa_client.grpc import Field
 from kuksa_client.grpc import VSSClientError
 from kuksa_client.grpc.aio import VSSClient
 
+
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         usage="-a [BROKER ADDRESS] -p [BROKER PORT] -f [FILE]",
         description="This provider writes the content of a csv file to a kuksa.val databroker",
     )
-    parser.add_argument("-a", "--address", default="127.0.0.1", help="This indicates the address of the kuksa.val databroker to connect to. The default value is 127.0.0.1")
-    parser.add_argument("-p", "--port", default="55555", help="This indicates the port of the kuksa.val databroker to connect to. The default value is 5555", type=int)
-    parser.add_argument("-f", "--file", default="signals.csv", help="This indicates the csv file containing the signals to update in the kuksa.val databroker. The default value is signals.csv.")
-    parser.add_argument("-i", "--infinite", action=argparse.BooleanOptionalAction, help="If the flag is set, the provider loops over the file until stopped, otherwise the file gets processed once.")
-    parser.add_argument("-l", "--log", default="WARNING", help="This sets the logging level. The default value is WARNING.", choices={"DEBUG", "INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"})
-    
+    parser.add_argument("-a", "--address", default="127.0.0.1", help="This indicates the address" +
+                        " of the kuksa.val databroker to connect to. The default value is 127.0.0.1")
+    parser.add_argument("-p", "--port", default="55555", help="This indicates the port" +
+                        " of the kuksa.val databroker to connect to. The default value is 5555", type=int)
+    parser.add_argument("-f", "--file", default="signals.csv", help="This indicates the csv file" +
+                        " containing the signals to update in the kuksa.val databroker." +
+                        " The default value is signals.csv.")
+    parser.add_argument("-i", "--infinite", action=argparse.BooleanOptionalAction,
+                        help="If the flag is set, the provider loops" +
+                        "the file until stopped, otherwise the file gets processed once.")
+    parser.add_argument("-l", "--log", default="INFO", help="This sets the logging level." +
+                        " The default value is WARNING.",
+                        choices={"INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"})
     return parser
+
 
 async def main():
     parser = init_argparse()
@@ -29,10 +38,11 @@ async def main():
     numeric_value = getattr(logging, args.log.upper(), None)
     if isinstance(numeric_value, int):
         logging.basicConfig(encoding='utf-8', level=numeric_value)
-    try: 
+    try:
         async with VSSClient(args.address, args.port) as client:
             csvfile = open(args.file, newline='')
-            signal_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            signal_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|', skipinitialspace=True)
+            logging.info("Starting to apply the signals read from %s.", str(csvfile.name))
             if args.infinite:
                 backup = list(signal_reader)
                 while True:
@@ -42,28 +52,32 @@ async def main():
             else:
                 await process_rows(client, signal_reader)
     except VSSClientError:
-        logging.error("Could not connect to the kuksa.val databroker at %s:%s. Make sure to set the correct connection details using --address and --port and that the kuksa.val databroker is running.", args.address, args.port)  
+        logging.error("Could not connect to the kuksa.val databroker at %s:%s. " +
+                      "Make sure to set the correct connection details using --address and --port" +
+                      "and that the kuksa.val databroker is running.", args.address, args.port)
+
 
 async def process_rows(client, rows):
     for row in rows:
-                entry = DataEntry(
-                    row[1],
-                    value=Datapoint(value=row[2]),
-                )
-                if row[0] == "current":
-                    updates = (EntryUpdate(entry, (Field.VALUE,)),)
-                    logging.info("Update current value of %s to %s", row[1], row[2])
-                elif row[0] == "target":
-                    updates = (EntryUpdate(entry, (Field.ACTUATOR_TARGET,)),)
-                    logging.info("Update target value of %s to %s", row[1], row[2])
-                else:
-                    updates = []
-                
-                try:
-                    await client.set(updates=updates)
-                except Exception as ex:
-                    logging.error("Error while updating %s\n%s", row[1], ex)
-                
-                await asyncio.sleep(int(row[3]))
-    
+        entry = DataEntry(
+            row['signal'],
+            value=Datapoint(value=row['value']),
+            )
+        if row['field'] == "current":
+            updates = (EntryUpdate(entry, (Field.VALUE,)),)
+            logging.info("Update current value of %s to %s", row['signal'], row['value'])
+        elif row['field'] == "target":
+            updates = (EntryUpdate(entry, (Field.ACTUATOR_TARGET,)),)
+            logging.info("Update target value of %s to %s", row['signal'], row['value'])
+        else:
+            updates = []
+        try:
+            await client.set(updates=updates)
+        except Exception as ex:
+            logging.error("Error while updating %s\n%s", row['signal'], ex)
+        try:
+            await asyncio.sleep(float(row['delay']))
+        except Exception:
+            logging.error("Error while waiting for '%s' seconds after updating %s.\n" +
+                          "Make sure to only use numbers for the delay value.", row['sleep'], row['delay'])
 asyncio.run(main())
