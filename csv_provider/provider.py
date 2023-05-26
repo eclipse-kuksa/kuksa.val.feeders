5bbb79f6c1ba683240b79fbd967de4e0274e5f5d
Sven Erik Jeroschewski <svenerik.jeroschewski@bosch.com>
2023-05-26 14:46:19 +0200

    adds Dockerfile and build workflow for CSV provider

--- csv_provider/provider.py
+++ csv_provider/provider.py
@@ -12,11 +12,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 ########################################################################
+'''A provider accepting VSS-signals from a CSV-file
+ to write these signals into an Kuksa.val data broker'''
 
 import asyncio
 import csv
 import argparse
 import logging
+import os
 
 from kuksa_client.grpc import Datapoint
 from kuksa_client.grpc import DataEntry
@@ -27,27 +30,35 @@ from kuksa_client.grpc.aio import VSSClient
 
 
 def init_argparse() -> argparse.ArgumentParser:
+    '''This inits the argument parser for the CSV-provider.'''
     parser = argparse.ArgumentParser(
         usage="-a [BROKER ADDRESS] -p [BROKER PORT] -f [FILE]",
         description="This provider writes the content of a csv file to a kuksa.val databroker",
     )
-    parser.add_argument("-a", "--address", default="127.0.0.1", help="This indicates the address" +
-                        " of the kuksa.val databroker to connect to. The default value is 127.0.0.1")
-    parser.add_argument("-p", "--port", default="55555", help="This indicates the port" +
-                        " of the kuksa.val databroker to connect to. The default value is 5555", type=int)
-    parser.add_argument("-f", "--file", default="signals.csv", help="This indicates the csv file" +
-                        " containing the signals to update in the kuksa.val databroker." +
-                        " The default value is signals.csv.")
-    parser.add_argument("-i", "--infinite", action=argparse.BooleanOptionalAction,
-                        help="If the flag is set, the provider loops" +
+    environment = os.environ
+    parser.add_argument("-a", "--address", default=environment.get("KUKSA_DATA_BROKER_ADDR",
+                                                                   "127.0.0.1"),
+                        help="This indicates the address of the kuksa.val databroker to connect to."
+                        " The default value is 127.0.0.1")
+    parser.add_argument("-p", "--port", default=environment.get('KUKSA_DATA_BROKER_PORT', "55555"),
+                        help="This indicates the port of the kuksa.val databroker to connect to."
+                        " The default value is 55555", type=int)
+    parser.add_argument("-f", "--file", default=environment.get("PROVIDER_SIGNALS_FILE",
+                                                                "signals.csv"),
+                        help="This indicates the csv file containing the signals to update in"
+                        " the kuksa.val databroker. The default value is signals.csv.")
+    parser.add_argument("-i", "--infinite", default=environment.get("PROVIDER_INFINITE"),
+                        action=argparse.BooleanOptionalAction,
+                        help="If the flag is set, the provider loops"
                         "the file until stopped, otherwise the file gets processed once.")
-    parser.add_argument("-l", "--log", default="INFO", help="This sets the logging level." +
-                        " The default value is WARNING.",
+    parser.add_argument("-l", "--log", default=environment.get("PROVIDER_LOG_LEVEL", "INFO"),
+                        help="This sets the logging level. The default value is WARNING.",
                         choices={"INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"})
     return parser
 
 
 async def main():
+    '''the main function as entry point for the CSV-provider'''
     parser = init_argparse()
     args = parser.parse_args()
     numeric_value = getattr(logging, args.log.upper(), None)
@@ -55,8 +66,11 @@ async def main():
         logging.basicConfig(encoding='utf-8', level=numeric_value)
     try:
         async with VSSClient(args.address, args.port) as client:
-            csvfile = open(args.file, newline='')
-            signal_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|', skipinitialspace=True)
+            csvfile = open(args.file, newline='', encoding="utf-8")
+            signal_reader = csv.DictReader(csvfile,
+                                           delimiter=',',
+                                           quotechar='|',
+                                           skipinitialspace=True)
             logging.info("Starting to apply the signals read from %s.", str(csvfile.name))
             if args.infinite:
                 backup = list(signal_reader)
@@ -67,12 +81,14 @@ async def main():
             else:
                 await process_rows(client, signal_reader)
     except VSSClientError:
-        logging.error("Could not connect to the kuksa.val databroker at %s:%s. " +
-                      "Make sure to set the correct connection details using --address and --port" +
-                      "and that the kuksa.val databroker is running.", args.address, args.port)
+        logging.error("Could not connect to the kuksa.val databroker at %s:%s."
+                      " Make sure to set the correct connection details using --address and --port"
+                      " and that the kuksa.val databroker is running.", args.address, args.port)
 
 
 async def process_rows(client, rows):
+    '''Processes a single row from the CSV-file and write the
+     recorded signal to the data broker through the client.'''
     for row in rows:
         entry = DataEntry(
             row['signal'],
@@ -88,11 +104,13 @@ async def process_rows(client, rows):
             updates = []
         try:
             await client.set(updates=updates)
-        except Exception as ex:
+        except VSSClientError as ex:
             logging.error("Error while updating %s\n%s", row['signal'], ex)
         try:
-            await asyncio.sleep(float(row['delay']))
-        except Exception:
-            logging.error("Error while waiting for '%s' seconds after updating %s.\n" +
-                          "Make sure to only use numbers for the delay value.", row['sleep'], row['delay'])
+            await asyncio.sleep(delay=float(row['delay']))
+        except ValueError:
+            logging.error("Error while waiting for %s seconds after updating %s to %s."
+                          " Make sure to only use numbers for the delay value.",
+                          row['delay'], row['signal'], row['value'])
+
 asyncio.run(main())
