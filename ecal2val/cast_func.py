dd102e634c9e07d47a81975470ea48c836d92090
lalywr2000 <12190450@inha.edu>
2024-01-10 17:04:22 +0100

    Add type casting function definition

---
+++ ecal2val/cast_func.py
@@ -0,0 +1,22 @@
+def string(value):
+    return str(value)
+
+
+def int32(value):
+    return int(value)
+
+
+def int64(value):
+    return int(value)
+
+
+def uint32(value):
+    return int(value)
+
+
+def uint64(value):
+    return int(value)
+
+
+def double(value):
+    return float(value)
